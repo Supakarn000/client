d8a6a3ea200e879c9a4f24e03dc485d5b6de65e5
Supakarn000 <s6404062616039@email.kmutnb.ac.th>
2023-10-14 16:25:00 +0700

    dd

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -14,6 +14,7 @@ const Navbar = () => {
         Cookies.remove("username");
         localStorage.removeItem("token");
         localStorage.removeItem("username");
+        localStorage.removeItem("cartItems");
         window.location.reload();
     };
 
